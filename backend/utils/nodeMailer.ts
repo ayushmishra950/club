20d0a97aa6da2475cb22876763e63cec01911016
ayushmishra950 <ayush950mishra@gmail.com>
2026-07-06 12:46:16 +0530

    email

--- backend/utils/nodeMailer.ts
+++ backend/utils/nodeMailer.ts
@@ -1,13 +1,16 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host : "smtp.gmail.com",
-    port : 465,
-    secure : true,
-    auth : {
-        user : process.env.EMAIL_USER,
-        pass : process.env.EMAIL_PASS
-    }
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
 });
 
 
